5db65bb898179f783752a4bd500756cc4a45a4d7
Simon Heath <icefoxen@gmail.com>
2014-07-02 08:29:11 -0400

    Doh, asset pairs already give me the base and quote.

--- krakker.py
+++ krakker.py
@@ -2,7 +2,7 @@
 import krakenex.api
 
 """Krakker: A test program to download market info from the kraken.com
-(crypto)currency exchange and look for profitable trades."""
+(crypto)currency exchange and look for profitable arbitrage trades."""
 
 def checkResponseError(query, args, response):
     err = response['error']
@@ -16,6 +16,9 @@ def doQuery(k, query, args):
     res = k.query_public(query, args)
     return checkResponseError(query, args, res)
 
+def getAssetInfo(k):
+    return doQuery(k, "Assets", '')
+
 def getAssetPairs(k):
     return doQuery(k, "AssetPairs", '')
 
@@ -23,17 +26,28 @@ def getTickers(k, pairs):
     pairstring = ','.join(pairs)
     return doQuery(k, "Ticker", {'pair' : pairstring})
 
-def getPrices(k):
-    pass
+def constructPotentialTrades(assets, assetpairs):
+    """Produces a table of, for each asset, what it can be traded for."""
+    trades = {}
+    for asset in assets.keys():
+        trades[asset] = []
+    for k,v in assetpairs.items():
+        base = v['base']
+        quote = v['quote']
+        trades[base].append(quote)
+
+    return trades
+
 
 def main():
     k = krakenex.API()
-    res = getAssetPairs(k)
+    assets = getAssetInfo(k)
+    assetPairs = getAssetPairs(k)
     #print('-----------ASSET PAIRS-------------------')
     #for key,val in res.items():
     #    print(key)
     #    print(val)
-    pairs = res.keys()
+    pairs = assetPairs.keys()
     tickers = getTickers(k, pairs)
     print('-----------TICKERS-----------------------')
     formstr = "{:<10}{:>15}{:>15}"
@@ -42,6 +56,7 @@ def main():
         bid = val['b'][0]
         ask = val['a'][0]
         print(formstr.format(key, bid, ask))
+    print(constructPotentialTrades(assets, assetPairs))
 
 if __name__ == '__main__':
     main()
